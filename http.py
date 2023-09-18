b408e243281a63f82a183a88176b3c820d58c742
irumaru <jibakomaorochi13@outlook.jp>
2023-09-18 13:49:11 +0000

    ファイル名と関数名の変更

--- http.py
+++ http.py
@@ -2,11 +2,8 @@ import subprocess
 from config import HTTP_CHECK_HOST_NAME, HTTP_CHECK_HOST_PORT, HTTP_CHECK_HOST_URI, HTTP_CHECK_HOST_TIMEOUT
 
 # 成功時return 0
-def httpCheck(hostIP):
+def checkOnline(hostIP):
     cmd = ["curl", "--max-time", HTTP_CHECK_HOST_TIMEOUT, "--resolve", HTTP_CHECK_HOST_NAME +":"+ HTTP_CHECK_HOST_PORT +":"+ hostIP, HTTP_CHECK_HOST_URI]
     result = subprocess.run(cmd, capture_output=True, text=True)
 
     return result.returncode
-
-status = httpCheck("124.110.25.63")
-print(status)
