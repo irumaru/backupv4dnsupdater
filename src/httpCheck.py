b311b105ea9b38c32c0500f4f4b94094661d2228
irumaru <jibakomaorochi13@outlook.jp>
2023-09-19 10:00:45 +0000

    curlのstatus codeをlogに出力

--- src/httpCheck.py
+++ src/httpCheck.py
@@ -1,13 +1,20 @@
 import subprocess
 import time
+import log
 from config import HTTP_CHECK_HOST_NAME, HTTP_CHECK_HOST_PORT, HTTP_CHECK_HOST_URI, HTTP_CHECK_HOST_TIMEOUT, HTTP_CHECK_HOST_RETRY
+
+curlLog = log.WiseLogger()
 
 # 成功時return 0
 def checkOnline(hostIP):
     cmd = ["curl", "--max-time", str(HTTP_CHECK_HOST_TIMEOUT), "--resolve", HTTP_CHECK_HOST_NAME +":"+ str(HTTP_CHECK_HOST_PORT) +":"+ hostIP, HTTP_CHECK_HOST_URI]
     result = subprocess.run(cmd, capture_output=True, text=True)
+    code = result.returncode
 
-    return result.returncode
+    if code != 0:
+        curlLog.print('Curl status code ' + str(code))
+
+    return code
 
 # HTTP_CHECK_HOST_RETRY回試行して接続できないときにFalseを返す
 def repeatCheckOnline(hostIP):
