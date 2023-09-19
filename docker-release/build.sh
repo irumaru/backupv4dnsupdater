9c6fcfe9c1d4553765e9bad063c86f6391633f06
irumaru <jibakomaorochi13@outlook.jp>
2023-09-19 08:06:17 +0000

    コンテナとビルダーの追加

---
+++ docker-release/build.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+# Delete old file.
+rm -r ../release/dist
+
+# Build
+docker compose build
+docker compose up
+
+# Copy config file.
+cp ../src/config.sample.yaml ../release/dist
+
+# Create priority file.
+touch ../release/dist/priority
+echo "PRIMARY" > ../release/dist/priority
+
+# Update owner.
+chown -R 1000:1000 ../release
