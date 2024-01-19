4011a766cea57858703e1a40e01384d8a020dda0
irumaru <jibakomaorochi13@outlook.jp>
2024-01-19 16:29:59 +0000

    httpsもチェックするように変更

--- src/httpCheck.py
+++ src/httpCheck.py
@@ -16,10 +16,21 @@ def checkOnline(hostIP):
 
     return code
 
+# 成功時return 0
+def checkOnlineHttps(hostIP):
+    cmd = ["curl", "-k", "--max-time", str(HTTP_CHECK_HOST_TIMEOUT), "--resolve", HTTP_CHECK_HOST_NAME +":"+ str(HTTP_CHECK_HOST_PORT) +":"+ hostIP, HTTP_CHECK_HOST_URI]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    code = result.returncode
+
+    if code != 0:
+        curlLog.print('Curl status code ' + str(code))
+
+    return code
+
 # HTTP_CHECK_HOST_RETRY回試行して接続できないときにFalseを返す
 def repeatCheckOnline(hostIP):
     for i in range(HTTP_CHECK_HOST_RETRY):
-        if checkOnline(hostIP) == 0:
+        if (checkOnline(hostIP) == 0 and checkOnlineHttps(hostIP) == 0):
             return True
         time.sleep(1)
     
